56d0f9ebafcd09aae7a2ed23f137afec3d19d491
Albert <albert.umcat@gmail.com>
2026-06-19 09:11:45 -0400

    Fix: dar feedback claro al cancelar trazo de linea en punto activo (chaining)

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@
  * Versión: debe actualizarse en cada deploy para invalidar caché.
  */
 
-const CACHE_NAME   = 'patronai-v21';
-const CACHE_STATIC = 'patronai-static-v21';
+const CACHE_NAME   = 'patronai-v22';
+const CACHE_STATIC = 'patronai-static-v22';
 
 // Assets que se cachean en la instalación
 const STATIC_ASSETS = [
