fa63a02407821a980c9dcb3c392ae6e3d1ebd4e9
Albert <albert.umcat@gmail.com>
2026-06-18 23:58:24 -0400

    Quitar línea recta heredada al ubicar punto con constructor A/B (queda libre, posición vive en la fórmula)

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@
  * Versión: debe actualizarse en cada deploy para invalidar caché.
  */
 
-const CACHE_NAME   = 'patronai-v5';
-const CACHE_STATIC = 'patronai-static-v5';
+const CACHE_NAME   = 'patronai-v6';
+const CACHE_STATIC = 'patronai-static-v6';
 
 // Assets que se cachean en la instalación
 const STATIC_ASSETS = [
