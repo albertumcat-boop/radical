41243168d5903ba55c47bbf9c13f3d9cda9fef85
Albert <albert.umcat@gmail.com>
2026-06-19 07:44:04 -0400

    Fix: priorizar línea sobre punto cercano al hacer clic, si el clic está geométricamente más cerca de la línea

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@
  * Versión: debe actualizarse en cada deploy para invalidar caché.
  */
 
-const CACHE_NAME   = 'patronai-v19';
-const CACHE_STATIC = 'patronai-static-v19';
+const CACHE_NAME   = 'patronai-v20';
+const CACHE_STATIC = 'patronai-static-v20';
 
 // Assets que se cachean en la instalación
 const STATIC_ASSETS = [
