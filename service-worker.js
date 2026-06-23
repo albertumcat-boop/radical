c0a1bc09fca58eb9a1c71fb98f35c18b4ea51e34
Albert <albert.umcat@gmail.com>
2026-06-22 23:07:33 -0400

    Agrega trazar sobre imagen de referencia en editor Trazar (dv6)

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@
  * Versión: debe actualizarse en cada deploy para invalidar caché.
  */
 
-const CACHE_NAME   = 'patronai-v29';
-const CACHE_STATIC = 'patronai-static-v29';
+const CACHE_NAME   = 'patronai-v30';
+const CACHE_STATIC = 'patronai-static-v30';
 
 // Assets que se cachean en la instalación
 const STATIC_ASSETS = [
